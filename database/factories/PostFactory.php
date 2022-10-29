e9c8401ccc5e1393c5bb27c0e00d9e42890850d1
Luis Aguilar <hi@luisae.dev>
2022-10-28 18:57:46 -0600

    feat: add factories and define tasks at seeder

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+
+    // Demonstrative videos
+    private array $videos = [
+        'https://www.youtube.com/watch?v=scJXVOPNQaM',
+        'https://www.youtube.com/watch?v=cvxjT2y5sQo',
+        'https://www.youtube.com/watch?v=3ZCXjUghgDg',
+        'https://www.youtube.com/watch?v=vhDbN1WZMo8',
+        'https://www.youtube.com/watch?v=wtkcADMkQwM',
+        'https://www.youtube.com/watch?v=SLLocWmL5E4',
+        'https://www.youtube.com/watch?v=sqtgBbem0iU',
+        'https://www.youtube.com/watch?v=5sLHCU7AzIU',
+        'https://www.youtube.com/watch?v=_Z7Yj09Cv1c',
+        'https://www.youtube.com/watch?v=H5ij1KN267M',
+        'https://www.youtube.com/watch?v=DQqJIZZ4d9o'
+    ];
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            // 'uuid' => $this->faker->unique()->uuid(),
+            'type' => $this->faker->randomElement(['msj', 'img', 'vid']),
+            'message' => $this->faker->text('280')
+        ];
+    }
+
+    /**
+    * Indicate the ref according to the post type.
+    *
+    * @return \Illuminate\Database\Eloquent\Factories\Factory
+    */
+    public function randomType()
+    {
+        return $this->state(function (array $attributes) {
+
+            switch($attributes['type']) {
+                case 'img':
+                    $attributes['ref'] = $this->faker->imageUrl(1200, 800, 'image', true);
+                    break;
+                case 'vid':
+                    $attributes['ref'] = $this->faker->randomElement($this->videos);
+                    break;
+            }
+            return $attributes;
+        });
+    }
+}
