4b87305014e07b8137744a3cfd5e2bb04e605307
Luis Aguilar <hi@luisae.dev>
2022-10-29 17:06:24 -0600

    feat: relations at pivot's model for followers

--- app/Models/Following.php
+++ app/Models/Following.php
@@ -5,9 +5,19 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Follower extends Model
+class Following extends Model
 {
     use HasFactory;
 
     protected $fillable = ['user_id', 'follow_user_id'];
+
+    public function follower()
+    {
+        return $this->belongsTo(User::class, 'id', 'follower_user_id');
+    }
+
+    public function followed()
+    {
+        return $this->belongsTo(User::class, 'id', 'followed_user_id');
+    }
 }
