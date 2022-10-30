0ee7f1d098db1343a0176143585a814f296848eb
Luis Aguilar <hi@luisae.dev>
2022-10-30 15:20:59 -0600

    misc: minor changes at some migrations

--- database/migrations/2022_10_28_195263_add_uuid_column_to_users_table.php
+++ database/migrations/2022_10_28_195263_add_uuid_column_to_users_table.php
@@ -2,8 +2,9 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-class AddUuidColumnToUsersTable extends Migration
+return new class extends Migration
 {
     /**
      * Run the migrations.
@@ -30,4 +31,4 @@ class AddUuidColumnToUsersTable extends Migration
             $table->dropColumn('uuid');
         });
     }
-}
+};
