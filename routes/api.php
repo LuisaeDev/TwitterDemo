235808ac72faea455c56fd8fd527f4e6e4e55158
Luis Aguilar <hi@luisae.dev>
2022-10-30 13:13:35 -0600

    feat: routes for tweets and followings

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\V1\Me\TweetController;
+use App\Http\Controllers\Api\V1\Me\FollowController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,24 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('ping', function() {
+    return 'pong';
+});
+
+Route::prefix('v1')->group(function () {
+
+    // Me / Tweets
+    Route::get('me/tweets',                [ TweetController::class, 'index' ]);
+    Route::post('me/tweet',                [ TweetController::class, 'create' ]);
+    Route::get('me/tweet/{tweet:uuid}',    [ TweetController::class, 'read' ]);
+    Route::delete('me/tweet/{tweet:uuid}', [ TweetController::class, 'delete' ]);
+
+    // Me / Following
+    Route::get('me/followers', [ FollowController::class, 'followers' ]);
+    Route::get('me/following', [ FollowController::class, 'following' ]);
+    Route::post('me/follow',   [ FollowController::class, 'follow' ]);
+    Route::post('me/unfollow', [ FollowController::class, 'unfollow' ]);
+
 });
