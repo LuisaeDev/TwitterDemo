d27b339f892aa674a12a650ec164d94392bb1738
Luis Aguilar <hi@luisae.dev>
2022-10-30 13:14:00 -0600

    misc: database seeder updated

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,7 +24,7 @@ class DatabaseSeeder extends Seeder
             ->count($nUsers)
             ->has(
                 Tweet::factory()
-                    ->count(rand(0, 20))
+                    ->count(rand(0, 100))
                     ->randomType()
             )
             ->create();
